7a95d18540443a785b8f3c97bcea98ba67e960ba
Luan <luanitachi@hotmail.com>
2025-09-16 18:21:37 -0300

    tape-adjustment

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -17,8 +17,8 @@ export const TapeSection = () => {
   return (
     <div className="py-16 overflow-x-clip lg:py-24">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1">
-        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] justify-center">
+          <div className="flex flex-none gap-4 py-3 ">
             {words.map((word) => (
               <div key={word} className="inline-flex gap-4 items-center">
                 <span className="text-gray-900 uppercase font-extrabold text-sm">
